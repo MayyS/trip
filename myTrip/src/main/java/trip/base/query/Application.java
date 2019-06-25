44508a5326fdde07bc08a9d9394b29a9e27e48f7
ssw <8648769006@qq.com>
2019-06-25 16:51:46 +0800

    springboot

---
+++ myTrip/src/main/java/trip/base/query/Application.java
@@ -0,0 +1,14 @@
+package trip.base.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String []args){
+        SpringApplication.run(Application.class,args);
+    }
+}
